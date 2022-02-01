11340159799108d8ecf31b6a7e0b5da3b90f7b1c
0x0015 <64482595+0x0015@users.noreply.github.com>
2022-01-31 20:36:51 -0500

    Release

--- ArguementHandler/Arguement.cpp
+++
@@ -1,7 +0,0 @@
-#include "Arguement.hpp"
-
-Arguement::Arguement(std::string t, std::string val){
-			type = t;
-			value = val;
-}
-
--- letterConvert.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-
-uint8_t noteFromLetter(std::string letter);
-std::string letterFromNote(uint8_t note, bool preferFlat = false);
-uint8_t isNoteFlat(std::string letter);//0 = neither, 2 = flat, 1 = sharp
